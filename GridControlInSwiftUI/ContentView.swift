17a6f531c875e261a12c963dd0d1acc1ac5e463c
Ramill <ramill_ibragimov@icloud.com>
2022-10-04 12:09:43 +0300

    screenshot

--- GridControlInSwiftUI/ContentView.swift
+++ GridControlInSwiftUI/ContentView.swift
@@ -9,11 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Grid {
+            GridRow {
+                GridCellView(systemName: "person", color: .orange)
+                GridCellView(systemName: "heart", color: .red)
+                    .gridCellColumns(2)
+            }
+            GridRow {
+                GridCellView(systemName: "airplane", color: .blue)
+                    .gridCellColumns(2)
+                GridCellView(systemName: "gear", color: .orange)
+            }
+            GridRow {
+                GridCellView(systemName: "app", color: .orange)
+                    .gridCellColumns(3)
+            }
+            GridRow {
+                GridCellView(systemName: "airplane", color: .orange)
+                GridCellView(systemName: "gear", color: .green)
+                GridCellView(systemName: "person", color: .orange)
+            }
         }
         .padding()
     }
@@ -22,5 +37,21 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct GridCellView: View {
+    let systemName: String
+    let color: Color
+    
+    var body: some View {
+        Image(systemName: systemName)
+            .resizable()
+            .frame(width: 50, height: 50)
+            .padding()
+            .frame(maxWidth: .infinity)
+            .background(in: RoundedRectangle(cornerRadius: 2))
+            .backgroundStyle(color.gradient)
+            .foregroundStyle(.white.shadow(.inner(radius: 1, x: 2, y: 2)))
     }
 }

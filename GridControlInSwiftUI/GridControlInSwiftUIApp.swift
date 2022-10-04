501cd9b3191e25661b5f280e3404483fb5e6e420
Ramill <ramill_ibragimov@icloud.com>
2022-10-04 11:57:21 +0300

    Initial Commit

---
+++ GridControlInSwiftUI/GridControlInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridControlInSwiftUIApp.swift
+//  GridControlInSwiftUI
+//
+//  Created by Ramill Ibragimov on 04.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct GridControlInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
